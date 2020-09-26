bdc2bb1b5aeef1ce8f706d26aae8f50334a3324e
Muhammad Shahroz Khan <32838158+shahroz94@users.noreply.github.com>
2020-09-26 08:12:43 +0500

    added material module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OneHaltMaterialModule } from './material-module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    OneHaltMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
